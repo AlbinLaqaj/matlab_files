edb72b82c549551c6c4a03650019c9aef43ba528
AlbinLaqaj <laqajalbin@gmail.com>
2024-07-14 18:21:36 +0200

    change commit

--- C_matrix.m
+++ C_matrix.m
@@ -4,7 +4,7 @@ function CM = C_matrix(q, q_dot)
 
     % Variablen aus q und q_dot extrahieren
     theta = q(3);
-    theta_dot = q_dot(3);
+    theta_dot = q_dot(3); %fdgsif
 
     % Elemente der C(q, q_dot)-Matrix berechnen
     CM = zeros(3,3); % Initialisiere eine 3x3-Matrix
