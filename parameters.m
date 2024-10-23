140da91890ae8c53a15f2e5619a7ffb17b835b05
AlbinLaqaj <laqajalbin@gmail.com>
2024-10-23 21:51:08 +0200

    update resistance R

--- parameters.m
+++ parameters.m
@@ -1,32 +1,29 @@
-% function parameters()
+% Define global parameters
+global m M g I J r l b t c d N R L Km eta param
 
-    % Define global parameters
-    global m M g I J r l b t c d N R L Km eta param
-    
-    % %% Räder- und Chassis-Parameter
-    
-    r = 0.044; % Radius Rad, [r] = m
-    m = 0.018 + 0.0095; % m_rad = 0,018 + m_d = 0,01, [m] = kg
-    M = 0.3303 - 2*m; % Masse Chassis, [M] = kg
-    l = 0.05; % Abstand Verbindugslinie und COM Chassis, [l] = m
-    t = 0.03; % Dicke des Chassis, [t] = m
-    b = 0.11; % Breite Chassis, [b] = m
-    g = 9.81; % Erdbeschleunigung, [g] = m/s^2
-    I = M/12*(t^2 + b^2) + M*l^2; % Massenträgheit Chassis, [I] = kg*m^2
-    J = 1/2*m*r^2; % Massenträgheit Rad, [J] = kg*m^2
-    d = 0.005; % Schätzung
-    c = 0.005; % Schätzung
-    
-    % %% Motorparameter
-    
-    N = 50; % Übersetzungsverhältnis, [N] = 1
-    R = 0.5; % Schätzung, Ankerwiderstand, [Ra] = Ohm
-    L = 2.6e-3; % Ankerinduktivität, [La] = H
-    Km = 0.05; % Schätzung, Motorkonstante, [Km] = V/rad/s 
-    eta = 0.9; % Schätzung, Getriebewirkungsgrad (90%), [eta] = 1
-    % tau_m = ;
-    
-    
-    param = [m; M; g; I; J; r; l; b; t; c; d; N; R; L; Km; eta];
+% %% Räder- und Chassis-Parameter
 
-% end
+r = 0.044; % Radius Rad, [r] = m
+m = 0.018 + 0.0095; % m_rad = 0,018 + m_d = 0,01, [m] = kg
+M = 0.3303 - 2*m; % Masse Chassis, [M] = kg
+l = 0.05; % Abstand Verbindugslinie und COM Chassis, [l] = m
+t = 0.03; % Dicke des Chassis, [t] = m
+b = 0.11; % Breite Chassis, [b] = m
+g = 9.81; % Erdbeschleunigung, [g] = m/s^2
+I = M/12*(t^2 + b^2) + M*l^2; % Massenträgheit Chassis, [I] = kg*m^2
+J = 1/2*m*r^2; % Massenträgheit Rad, [J] = kg*m^2
+d = 0.005; % Schätzung
+c = 0.005; % Schätzung
+
+
+% %% Motorparameter
+
+N = 50; % Übersetzungsverhältnis, [N] = 1
+R = 12.5; % Messung, Ankerwiderstand, [Ra] = Ohm
+L = 2.6e-3; % Ankerinduktivität, [La] = H
+Km = 0.224; % Schätzung, Motorkonstante, [Km] = V/rad/s 
+eta = 0.9; % lower, Schätzung, Getriebewirkungsgrad, [eta] = 1
+tau_m = 0.005; % Schätzung, Überbrückungsmoment, [tau_m] = Nm
+
+
+param = [m; M; g; I; J; r; l; b; t; c; d; N; R; L; Km; eta];
