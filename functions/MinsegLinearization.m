516b01c667e674797acdb1f152de2ec3aac58b51
AlbinLaqaj <laqajalbin@gmail.com>
2024-11-21 23:42:45 +0100

    add the 3 _sym functions

--- functions/MinsegLinearization.m
+++ functions/MinsegLinearization.m
@@ -41,3 +41,47 @@ function [A, B, C, D] = MinsegLinearization()
     C = eye(6);
     D = zeros(6,2);
 end
+
+
+% help function for computing M_Matrix
+function MM = MM_sym(q, r, m, M, l, I, J)
+
+    % Variablen aus q extrahieren
+    theta = q(3);
+
+    % Elemente der M(q)-Matrix berechnen
+    MM(1,1) = r^2*(1/4*M + m) + J;
+    MM(1,2) = 1/4*M*r^2;
+    MM(1,3) = r^2*(1/2*M + m) + J + 1/2*M*l*r*cos(theta);
+    MM(2,1) = 1/4*M*r^2;
+    MM(2,2) = r^2*(1/4*M + m) + J;
+    MM(2,3) = r^2*(1/2*M + m) + J + 1/2*M*l*r*cos(theta);
+    MM(3,1) = r^2*(1/2*M + m) + J + 1/2*M*l*r*cos(theta);
+    MM(3,2) = r^2*(1/2*M + m) + J + 1/2*M*l*r*cos(theta);
+    MM(3,3) = r^2*(M + 2*m) + 2*J + 2*M*l*r*cos(theta) + M*l^2 + I;
+end
+
+
+% help function for computing C_Matrix
+function CM = CM_sym(x, r, M, l)
+
+    % Variablen aus in = [q q_dot] extrahieren
+    theta = x(3);
+    theta_dot = x(6);
+
+    % Elemente der C(q, q_dot)-Matrix berechnen
+    CM(1,3) = -1/2*M*l*r*theta_dot*sin(theta);
+    CM(2,3) = -1/2*M*l*r*theta_dot*sin(theta);
+    CM(3,3) = -M*l*r*theta_dot*sin(theta);
+end
+
+
+% help function for computing G_Matrix
+function GM = GM_sym(q, M, g, l)
+
+    % Variablen aus q extrahieren
+    theta = q(3);
+
+    % Elemente der G(q)-Matrix berechnen
+    GM = [0; 0; -M*g*l*sin(theta)];
+end
